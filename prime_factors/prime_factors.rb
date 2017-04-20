2db8cf6b91ac963c22d1dd70891c1e16568624f7
Mark Siemers <mark.siemers@gmail.com>
2017-04-19 19:21:40 -0700

    Refactor prime factors slightly

--- prime_factors/prime_factors.rb
+++ prime_factors/prime_factors.rb
@@ -1,3 +1,5 @@
+require 'prime'
+
 def prime_factors(number)
   return [] if number < 2
 
@@ -5,37 +7,32 @@ def prime_factors(number)
   [prime_factor] + prime_factors(number / prime_factor)
 end
 
-def lowest_prime_factor(number)
-  return nil if number < 2
-
-  possible_factors_not_one_and_self(number).each do |possible_factor|
-    if prime?(possible_factor) and evenly_divisible?(number, possible_factor)
-      return possible_factor
-    end
-  end
-
-  number
+def prime_factor?(number,factor)
+  evenly_divisible?(number, factor) && factor.prime?
 end
 
-def possible_factors_not_one_and_self(number)
-  if number <= 2
-    []
-  else
-    end_of_range = [(number / 2), 2].max
-    (2..end_of_range).to_a
-  end
+def lowest_prime_factor(number)
+  return nil if number < 2
+  possible_factors(number).find{|factor| prime_factor?(number, factor) } || number
+end
+
+def possible_factors(number)
+  return [] if number <= 2
+  return [2] if number == 3
+  (2..(number / 2)).to_a
 end
 
 def prime?(number)
-  return false if number < 2
-
-  possible_factors_not_one_and_self(number).each do |possible_factor|
-    return false if evenly_divisible?(number, possible_factor)
-  end
-
-  true
+  # return false if number < 2
+  #
+  # possible_factors_not_one_and_self(number).each do |possible_factor|
+  #   return false if evenly_divisible?(number, possible_factor)
+  # end
+  #
+  # true
+  number.prime?
 end
-
+#
 def evenly_divisible?(numerator, denominator)
   (numerator % denominator).zero?
 end
